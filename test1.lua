fa600f7fef02276c64d3395f87a0b14ffacd8b4b
ClockGlock <69520355+ClockGlock@users.noreply.github.com>
2021-04-16 17:59:16 +1000

    Update test1.lua

--- test1.lua
+++ test1.lua
@@ -1,5 +1,5 @@
 local rand = Random.new(os.time()):NextInteger(1, 100000)
 
 while wait(0.5) do
-    print("IM ANNOYING!!!", rand)
+    print("IM jpmohygrejopighjioprjg854hjv-945hv9084735!!!", rand)
 end
